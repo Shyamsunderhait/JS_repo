cb00a2b0aaba631a829bca8d18a4dc970a6dcb61
Shyamsunder Hait <88630084+Shyamsunderhait@users.noreply.github.com>
2024-01-15 19:33:57 +0000

    datatypes

---
+++ Basics/03_Datatypes.js
@@ -0,0 +1,17 @@
+let name = "Shyam"
+let age = 18
+let loggedIn = true
+
+// number = 2 to power 53
+// bigInt
+// string
+// boolean = true/false
+// null = standalone value
+// undefined = let name;
+// symbol = unique
+
+//object
+
+console.log(typeof age)
+console.log(typeof null); //object
+console.log(typeof undefined) // undefined
