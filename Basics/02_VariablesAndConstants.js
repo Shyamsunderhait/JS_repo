aed903d9acffafb6f9325fcd82459abb7d56396f
Shyamsunder Hait <88630084+Shyamsunderhait@users.noreply.github.com>
2024-01-15 19:33:49 +0000

    variables

---
+++ Basics/02_VariablesAndConstants.js
@@ -0,0 +1,9 @@
+const accountId = 45637
+let accountEmail = "shyamsunderhait@gmail.com"
+var accountAddress = "Mumbai"
+accountType = "demat account"
+
+
+
+
+console.log([accountId,accountAddress,accountEmail,accountType]);
