f4ec02b0b3137d4fba2ae317d9bd40614ca3ef1c
Shyamsunder Hait <88630084+Shyamsunderhait@users.noreply.github.com>
2024-01-17 11:02:03 +0000

    datatype summary

--- Basics/03_Datatypes.js
+++ Basics/03_Datatypes.js
@@ -15,3 +15,40 @@ let loggedIn = true
 console.log(typeof age)
 console.log(typeof null); //object
 console.log(typeof undefined) // undefined
+
+// datatypes basics
+// Primitive(call by value)
+
+// 7 types : String, Number, Boolean, Null, 
+//Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+console.log(id);
+
+const bigNumber = 465456165846413456n 
+
+// Reference type(non primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["Spiderman", "iron man", "black panther"]
+let myObj = {
+    name:"shyam",
+    age:21
+}
+
+const myFunction = function(){
+    console.log("shyamsunderhait");
+}
+
+console.log(typeof myObj)
+
